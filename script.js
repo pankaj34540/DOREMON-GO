8d36d54be69218e7dbd06aa7c5d053411327741a
PANKAJ <128164127+pankaj34540@users.noreply.github.com>
2024-12-20 21:36:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const player = document.getElementById('player');
+const scoreDisplay = document.getElementById('score');
+const levelDisplay = document.getElementById('level');
+const jumpSound = document.getElementById('jumpSound');
+const gameOverSound = document.getElementById('game
